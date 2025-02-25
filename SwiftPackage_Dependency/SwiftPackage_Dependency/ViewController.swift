bf3c47925a87a711eafd9f00f4f7298687a6053b
vivekwsnk <vivekwsnk3@gmail.com>
2025-02-25 23:07:54 +0530

    First commint and add dependency versions

---
+++ SwiftPackage_Dependency/SwiftPackage_Dependency/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftPackage_Dependency
+//
+//  Created by vivally iosdev on 2/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //
+    }
+
+
+}
+
